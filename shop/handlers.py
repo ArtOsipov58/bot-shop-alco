8a0b6a743c833c985758d2986a00b187c2010315
Alex <abelockii80@mail.ru>
2021-02-21 16:03:40 +0300

    Сделал нормальную навигацию по меню

--- shop/handlers.py
+++ shop/handlers.py
@@ -26,23 +26,49 @@ def get_my_id(update, context):
 
 
 def select_category(update, context):
-    update.message.reply_text(
+    msg = update.message.reply_text(
         'Выберите категорию',
-        reply_markup=get_cat_ikb())
+        reply_markup=menu.get_cat_ikb()
+        )
+    context.user_data['msg_id'] = msg.message_id
+
+
+def get_cat_list(update, context):
+    query = update.callback_query
+    cat_id = int(query.data.split('_')[-1])
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    category = session.query(Category).filter_by(id=cat_id).first()
+    context.user_data['cat_id'] = cat_id
+
+    context.bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=context.user_data['msg_id'],
+        text=f'Категория: <b>{category.name}</b>',
+        reply_markup=menu.get_product_ikb(category.product_list)
+        )
 
 
 def select_product(update, context):
     query = update.callback_query
+    screen_num = int(query.data.split('_')[-1])
     Session = sessionmaker(bind=engine)
     session = Session()
-    # product = session.query(Product).filter_by(cat_id=int(query.data.split('_')[-1])).all()
-    category = session.query(Category).filter_by(id=int(query.data.split('_')[-1])).first()
-    query.message.reply_text(
-        f'Категория: <b>{category.name}</b>',
-        reply_markup=get_product_ikb(category.product_list)
+
+    cat_id = context.user_data['cat_id']
+    category = session.query(Category).filter_by(id=cat_id).first()
+    context.bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=context.user_data['msg_id'],
+        text=f'Категория: <b>{category.name}</b>',
+        reply_markup=menu.get_product_ikb(category.product_list, screen_num=screen_num)
         )
 
 
 def get_product_cart(update, context):
     query = update.callback_query
-    query.message.reply_text('Вот карточка товара')
+    context.bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=context.user_data['msg_id'],
+        text='Вот карточка товара'
+        )
--- shop/keyboard.py
+++ shop/keyboard.py
@@ -15,44 +15,15 @@ def get_main_menu():
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 
-def get_cat_ikb():
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    ikb_list = list()
-    for cat in session.query(Category).order_by(Category.order):
-        ikb_list.append(InlineKeyboardButton(cat.name, callback_data=f'cat_{str(cat.id)}'))
-    menu = build_menu(ikb_list, 3)
-    return InlineKeyboardMarkup(menu)
-
-
-def get_product_ikb(product_list):
-    ikb_list = list()
-    for product in product_list:
-        ikb_list.append(InlineKeyboardButton(product.name, callback_data=f'prod_{str(product.id)}'))
-
-    menu = build_menu(ikb_list, 2)
-    return InlineKeyboardMarkup(menu)
-
-
-
-def build_menu(buttons,
-            n_cols,
-            header_buttons=None,
-            footer_buttons=None):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, [header_buttons])
-    if footer_buttons:
-        menu.append([footer_buttons])
-    return menu
 
 
 
 class Menu:
+    def __init__(self):
+        self.len_one_screen = 5
 
-    def build_menu(self, 
-                buttons,
+    @staticmethod
+    def _build_menu(buttons,
                 n_cols,
                 header_buttons=None,
                 footer_buttons=None):
@@ -63,10 +34,70 @@ class Menu:
             menu.append([footer_buttons])
         return menu
 
+    @staticmethod
+    def _split_list(kblist, step):
+        result_list = []
+        for i in range(step, len(kblist) + step, step):
+            if i == step:
+                s_list = kblist[:i]
+                result_list.append(s_list)
+            else:
+                split_list = kblist[(i-step) : i]
+                result_list.append(split_list)
+        return result_list   # Список из списков
 
 
-# buttons = []
+    @staticmethod
+    def get_cat_ikb():
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        ikb_list = list()
+        for cat in session.query(Category).order_by(Category.order):
+            ikb_list.append(InlineKeyboardButton(cat.name, callback_data=f'cat_{str(cat.id)}'))
+        menu = Menu._build_menu(ikb_list, 3)
+        return InlineKeyboardMarkup(menu)
+
+
+
+    @staticmethod
+    def _get_ikb_list(product_list):
+        ikb_list = list()
+        for product in product_list:
+            ikb_list.append(InlineKeyboardButton(product.name, callback_data=f'prod_{str(product.id)}'))
+        return ikb_list
 
 
 
 
+
+    def get_product_ikb(self, product_list, screen_num=1):
+        ikb_list = Menu._get_ikb_list(product_list)
+        screen_list = Menu._split_list(ikb_list, 5)
+        screen_dict = dict()
+        for screen in screen_list:
+            screen_dict[str(screen_list.index(screen) + 1)] = screen
+
+        raw_menu = screen_dict[str(screen_num)]
+        menu = Menu._build_menu(raw_menu, 1)
+
+
+        # import ipdb; ipdb.set_trace()
+
+
+        if screen_num == 1:
+            menu.append([InlineKeyboardButton('>>>', callback_data=f'nav_{str(screen_num+1)}')])
+        elif 1 < screen_num < len(screen_dict):
+
+            menu.append(
+                [InlineKeyboardButton('<<<', callback_data=f'nav_{str(screen_num-1)}'), 
+                 InlineKeyboardButton('>>>', callback_data=f'nav_{str(screen_num+1)}')]
+                )
+        elif screen_num == len(screen_dict):
+            menu.append(
+                [InlineKeyboardButton('<<<', callback_data=f'nav_{str(screen_num-1)}')]
+                )
+        return InlineKeyboardMarkup(menu)
+
+
+menu = Menu()
